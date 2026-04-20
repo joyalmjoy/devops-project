8e12641313af5a4bae2e8b9df3ed9adc700a4a7d
Romy Babu <rbabu12@my.centennialcollege.ca>
2026-04-19 22:12:28 -0400

    Fix button text update bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMessage() {
   const status = document.getElementById("status");
   if (status) {
-    status.textContent = "Recipient feature is working properly now.";
+    status.textContent = "Recipient feature fixed and working properly!";
   }
 }
